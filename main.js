869f41a0fc0ebdb4ae7eaf99cce5e46d0dc14747
meherkothari8129 <87307807+meherkothari8129@users.noreply.github.com>
2021-09-20 17:08:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+prediction = "";
+
+Webcam.set
+(
+    {
+        width:350,
+        height:300,
+        image_format : 'png',
+        png_quality:90
+    }
+);
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/QlCHxqd-5/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = "The Gesture you are showing is "+ prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis); 
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else 
+    {
+       console.log(results);
+
+       document.getElementById("result_gesture_name").innerHTML = results[0].label;
+       prediction = results[0].label;
+
+       speak()
+
+        if(results[0].label == "Thumbs_up")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128522;";
+        }
+        if(results[0].label == "Very_good")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128532;";
+        }
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128548;";
+        }
+    }
+}
